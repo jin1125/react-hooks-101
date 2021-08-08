5a772533c0ec93117478357a8a1852452ae15582
jin1125 <shogo.jinta@gmail.com>
2021-08-08 20:25:44 +0900

    implemet all events deletion feature

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 const events = (state = [], action) => {
   switch (action.type) {
     case "CREATE_EVENT":
-      const event = { titile: action.title, body: action.body };
+      const event = { title: action.title, body: action.body };
       const length = state.length;
       const id = length === 0 ? 1 : state[length - 1].id + 1;
       return [...state, { id, ...event }];
     case "DELEATE_EVENT":
-      return state;
+      return state.filter(event=>event.id !== action.id )
     case "DELEATE_ALL_EVENTS":
       return [];
     default:
