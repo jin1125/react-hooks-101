7443dba71bfc2381cc1b337520f78369ed2dd005
jin1125 <shogo.jinta@gmail.com>
2021-08-08 16:17:17 +0900

    create-reducer-for-events

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+const events = (state = [], action) => {
+  switch (action.type) {
+    case "CREATE_EVENT":
+      const event = { titile: action.title, body: action.body };
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, { id, ...event }];
+    case "DELEATE_EVENT":
+      return state;
+    case "DELEATE_ALL_EVENTS":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default events;
